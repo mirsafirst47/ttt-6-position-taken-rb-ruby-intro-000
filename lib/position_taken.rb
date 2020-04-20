364e97a7c82c91fc1a1a6f5ddb3f2bc8413eb85f
Samir Triande <habib.triande@gmail.com>
2020-04-20 04:53:11 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] == "X" || board[index] == "O"
+  if board[index] == "1" || board[index] == "0"
     "true"
   elsif board[index] != ""
     !true
