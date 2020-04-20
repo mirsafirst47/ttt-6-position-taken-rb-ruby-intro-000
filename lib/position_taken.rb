f977314b115ac007b84816e784812b4220dd8204
Samir Triande <habib.triande@gmail.com>
2020-04-20 04:40:18 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
   if board[index] != ""
-     "false"
+     !true
   elsif board[index] != " "
     puts "false"
   else
