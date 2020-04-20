eb8bf521fcb7bed5385548511942d13080483137
Samir Triande <habib.triande@gmail.com>
2020-04-20 04:48:52 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -4,7 +4,7 @@ def position_taken?(board, index)
     !true
   elsif board[index] != " "
     !true
-  elsif board[index] == "X" || board[index] == "O"
-    true
+  elsif board[index] != "X" || board[index] != "O"
+    !true
   end
 end
