551c010a007f781c2d759cee9de8a16fefe01ed4
Samir Triande <habib.triande@gmail.com>
2020-04-20 05:15:04 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@ def position_taken?(board, index)
   if board[index] != " " || board[index] != "" || board[index] != nil
     !true
   elsif board[index] == "0" || board[index] == "1"
-    "true"
+    !=false
 
 
 
